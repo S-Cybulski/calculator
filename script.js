767a8e722aa56da01dd774a4290d24e1ef0ae775
S-Cybulski <szymcyb04@protonmail.com>
2023-09-19 17:15:56 +0100

    added operator function and setup html

--- script.js
+++ script.js
@@ -5,3 +5,24 @@ const subtract = (num1, num2) => num1 - num2;
 const multiply = (num1, num2) => num1 * num2;
 
 const divide = (num1, num2) => num1 / num2;
+
+const operate = function(operator, firstNum, lastNum){
+    switch(operator){
+        case "+":
+            return add(firstNum, lastNum);
+        case "-":
+            return subtract(firstNum, lastNum);
+        case "*":
+            return multiply(firstNum, lastNum);
+        case "/":
+            return divide(firstNum, lastNum);
+        default:
+            return undefined;
+    }
+}
+
+let firstNum;
+let operator;
+let lastNum;
+
+console.log(operate("-",24,8));
